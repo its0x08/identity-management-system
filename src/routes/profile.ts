29c108e16217b0b5514458165576dbdd9c889b9d
its0x08 <m.thg@icloud.com>
2022-02-25 20:07:43 +0300

    fix: Put auth middleware on each route handler

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,20 +1,18 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response } from 'express';
 import { checkToken } from '../middlewares/checkAuth';
 import User from '../models/users';
 
 
 const router = express.Router();
 
-router.use(checkToken);
-
 router
 	.route('/profile')
-	.get(async (req: Request, res: Response) => {
+	.get(checkToken, async (req: Request, res: Response) => {
         // @ts-ignore
         const user = await User.findOne({ email: req.user.email });
         return res.json(user.toJSON());
     })
-    .put((req: Request, res: Response) => {
+    .put(checkToken, (req: Request, res: Response) => {
         return res.json({});
     });
 
