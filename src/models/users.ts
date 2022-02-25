a137c05c8f0e3b97b25996633d3172be4a9e746f
its0x08 <m.thg@icloud.com>
2022-02-25 19:49:04 +0300

    feat: Add user model

---
+++ src/models/users.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    email: { type: String, unique: true, required: true },
+    password: String,
+    firstName: String,
+    lastName: String,
+    birthDate: Date,
+    lastLogin: Date,
+    createdAt: Date,
+    updatedAt: Date,
+},
+    {
+        toJSON: {
+            transform: function (doc, ret) {
+                delete ret._id;
+                delete ret.__v;
+                delete ret.password;
+            }
+        }
+    }
+);
+
+userSchema.virtual('fullName').get(function () {
+    // @ts-ignore
+    return this.firstName + ' ' + this.lastName;
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
