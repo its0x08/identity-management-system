e46e980f88674602b074215e4444e2ada654cc8c
its0x08 <m.thg@icloud.com>
2022-02-25 20:27:49 +0300

    feat: Add PUT /profile route

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -8,12 +8,31 @@ const router = express.Router();
 router
 	.route('/profile')
 	.get(checkToken, async (req: Request, res: Response) => {
-        // @ts-ignore
-        const user = await User.findOne({ email: req.user.email });
-        return res.json(user.toJSON());
-    })
-    .put(checkToken, (req: Request, res: Response) => {
-        return res.json({});
-    });
+		// @ts-ignore
+		const user = await User.findOne({ email: req.user.email });
+		return res.json(user.toJSON());
+	})
+	.put(checkToken, async (req: Request, res: Response) => {
+		// @ts-ignore
+		const user = await User.findOne({ email: req.user.email });
+		if (!user) {
+			return res.status(404).json({
+				status: 'failed',
+				errors: ['user not found']
+			});
+		}
+
+		if(req.body.firstName){
+			user.firstName = req.body.firstName;
+		}
+		if(req.body.lastName){
+			user.lastName = req.body.lastName;
+		}
+		if(req.body.birthDate){
+			user.birthDate = req.body.birthDate;
+		}
+		const updatedUser = await user.save();
+		return res.json(updatedUser.toJSON());
+	});
 
 export default router;
