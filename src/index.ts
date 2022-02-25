5a8e811942eb167b96bc2b8645e909b4906fd359
its0x08 <m.thg@icloud.com>
2022-02-25 19:53:43 +0300

    feat: Add initial express backend

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response, NextFunction } from 'express';
+import 'dotenv/config';
+import expressLogger from './utils/logger';
+
+import mongooseConnection from './utils/mongoose';
+console.log('Mongoose state:', mongooseConnection.connection.readyState);
+
+const app = express();
+
+app.use(express.json());
+app.use(expressLogger);
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('Hello World');
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	return res.status(500).json({
+		message: 'failed',
+		errors: [err.message]
+	});
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Servers started on http://localhost:${port}`));
