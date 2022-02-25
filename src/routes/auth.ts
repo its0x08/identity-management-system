8c42f5c931015f31d80febdabd6c5e77bbad2568
its0x08 <m.thg@icloud.com>
2022-02-25 19:57:50 +0300

    feat: Add login route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import Joi from 'joi';
 import User from '../models/users';
-import { hash } from '../utils/bcrypt';
+import { compareHash, hash } from '../utils/bcrypt';
+import { generateToken } from '../utils/jwt';
 
 
 
@@ -14,6 +15,14 @@ const validateUser = (body: any) => {
         firstName: Joi.string().min(4).max(30),
         lastName: Joi.string().min(4).max(30),
         birthDate: Joi.date().required(),
+    });
+    return schema.validate(body);
+}
+
+const validateLogin = (body: any) => {
+    const schema = Joi.object({
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).max(40),
     });
     return schema.validate(body);
 }
@@ -54,5 +63,35 @@ router
         res.json({ message: 'created', errors: [] });
     });
 
+
+router
+    .route('/login')
+    .post(async (req: Request, res: Response) => {
+        if (!req.body) {
+            return res.status(400).json({
+                status: 'failed',
+                errors: [
+                    'Data is required'
+                ]
+            });
+        }
+        const { error } = validateLogin(req.body);
+        if (error) {
+            return res.status(400).json({
+                status: 'failed',
+                errors: error.details.map(e => e.message)
+            });
+        }
+        const user = await User.findOne({ email: req.body.email });
+
+        if (!user) return res.status(404).json({ status: 'not found' });
+
+        const hashMatch = await compareHash(req.body.password, user.password);
+        if (!hashMatch) return res.status(400).json({ status: 'wrong password' });
+
+        const userJson = user.toJSON();
+        const token = generateToken(userJson);
+        return res.json({ message: 'success', user: userJson, token});
+    });
 
 export default router;
